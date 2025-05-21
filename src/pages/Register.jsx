d9b15d9c6f243d07dc495617d59c1a7d1a251f40
mihalisdgolias <mihalisdgolias@gmail.com>
2025-05-21 16:25:02 +0300

    register file update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,7 +19,7 @@ export default function Register() {
     }
 
     try {
-      const res = await fetch('${import.meta.env.VITE_API_BASE}/api/auth/register', {
+      const res = await fetch(`${import.meta.env.VITE_API_BASE}/api/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
